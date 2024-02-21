2f63f386ade8061922eb661b9950e30dd3f9539f
Jeremy Prieto <josue2002_prieto@hotmail.com>
2024-02-21 12:07:27 -0500

    firstcommitlinux

--- src/app/Componentes/Bus-Componente/listar-bus/listar-bus.component.ts
+++ src/app/Componentes/Bus-Componente/listar-bus/listar-bus.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Bus } from '../Entidades/Bus';
+import { DbBusService } from '../Service/db-bus.service';
 
 @Component({
   selector: 'app-listar-bus',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './listar-bus.component.css'
 })
 export class ListarBusComponent {
+  @Output() editarClienteEvent = new EventEmitter<Bus>();
+  get ListaBuses() {
+    return this.obs.getListaChofer;
+  }
+  EditarBus(chofer: Bus) {
+    this.editarClienteEvent.emit(chofer);
+  }
+  EliminarBus(chofer: Bus) {
+    this.obs.DeleteBus(chofer.codigo);
+  }
 
+  constructor(private obs: DbBusService) {
+
+  }
+  ngOnInit(): void {
+
+
+  }
 }
